65ca9ef4173fd3d8e1c192d2297f55c2559e5897
juantipismana <juan.tipismana@softwareandina.com>
2018-02-01 15:46:20 -0500

    add 01022018

---
+++ Branch2018/test1/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace test1
+{
+    public class Customer
+    {
+        public Customer()
+        {
+        }
+
+        public int CustomerId { get; set; }
+
+        public string FullName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        public int MonthBirthday { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FullName} {this.LastName}";
+        }
+    }  
+}
