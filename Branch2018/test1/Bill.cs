198f51c120a637fb8c2ea14a2ac6f6af73e8b9a4
juantipismana <juan.tipismana@softwareandina.com>
2018-02-01 15:46:02 -0500

    add 01022018

---
+++ Branch2018/test1/Bill.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace test1
+{
+    public class Bill
+    {
+        public Bill()
+        {
+        }
+
+        public int BillId { get; set; }
+
+        public int SellerId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int CodeBill { get; set; }
+
+        public int NumberRUC { get; set; }
+
+        public string DateEmision { get; set; }
+
+        public string DateExpiration { get; set; }
+
+        public string NameCompany { get; set; }
+        
+        public int AmountTotal { get; set; }
+    }
+}
